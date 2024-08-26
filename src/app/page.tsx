788530e9fb7ac1c66b8c3b306ccbf25ee99f958a
Tashdik <s2110477153@ru.ac.bd>
2024-08-26 11:01:29 +0600

    navItems added in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <div>
         {/* <FloatingNavDemo /> */}
         {/* <Navbar /> */}
-        <FloatingNav />
+        <FloatingNav navItems={navItems}/>
         <BlurIn word={"lorem"}></BlurIn>
         <OrbitingCirclesDemo />
         <SparklesPreview />
@@ -23,6 +23,26 @@ export default function Home() {
     </main>
   );
 }
+
+
+const navItems = [
+  {
+    name: "Home",
+    link: "/",
+  //   icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "About",
+    link: "/about",
+  //   icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+  //   icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+    
+  },
+];
 
 
 
